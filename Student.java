948498739563fee64f7c2f58e4bab4dd52cbd65c
Abdul Ahad Shahid <abdulahadshahid959@gmail.com>
2025-09-10 15:40:15 +0500

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,15 +1,55 @@
-public class Student{
- 
-  String name;
-  String email;
-  double gpa;
- public void display (){
-    System.out.println("Method is called .");
+// Abdul Ahad Shahid
+// Sp25-Bcs-001
 
-}
+public class Student {
+
+   
+    private String name;
+    private String email;
+    private double gpa;
+
+   
+    public Student(String name, String email, double gpa) {
+        this.name = name;
+        this.email = email;
+        this.gpa = gpa;
+    }
 
 
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+    public double getGpa() {
+        return gpa;
+    }
+
+   
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+   
+    public void display() {
+       
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("GPA: " + gpa);
+    }
 }
